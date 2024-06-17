2b7037eb4fa8b42e2b601d6b05a9021966cc1549
Harrison <harrisonchialuka@gmail.com>
2024-06-17 16:04:44 +0100

    task 0 - 4

--- web_dynamic/3-hbnb.py
+++ web_dynamic/3-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/1-hbnb', strict_slashes=False)
+@app.route('/3-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     cache_id = uuid()
@@ -36,12 +36,10 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('1-hbnb.html',
+    return render_template('3-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places,
-                           cache_id=cache_id
-                           )
+                           places=places, cache_id=cache_id)
 
 
 if __name__ == "__main__":
